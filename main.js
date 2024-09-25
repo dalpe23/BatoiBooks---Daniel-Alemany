ef5c216d2298c85cd0c751e20fc9b06a72abd1a8
Daniel Alemany <dalpe2003@gmail.com>
2024-09-25 15:51:42 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+      <img src="./public/logoBatoi.png" class="logo" alt="Vite logo" />
+    </a>
+    <h1>BatoiBooks</h1>
+    <p>
+      Abre la consola para ver el resultado
+    </p>
+  </div>
+`
